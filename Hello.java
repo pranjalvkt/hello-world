cf42710b97c281749cbf7d06e1eeae18a7d10553
pranjalvkt <64363696+pranjalvkt@users.noreply.github.com>
2020-04-27 13:36:32 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,15 @@
+public class Hello
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello pranjal");
+
+        int myNumber = (5 + 10) + (10 * 2);
+        int mySecondNumber = 12;
+        int myThirdNumber = myNumber * 2;
+        int myTotal = myNumber + mySecondNumber + myThirdNumber;
+        System.out.println(myTotal);
+        
+   
+    }
+}
